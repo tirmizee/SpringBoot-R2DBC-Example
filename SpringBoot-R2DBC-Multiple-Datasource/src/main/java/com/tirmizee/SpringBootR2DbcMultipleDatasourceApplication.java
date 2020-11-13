1500a32f9054eaf724d7cdacf39bdade5d4277de
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-11-13 14:44:49 +0700

    init SpringBoot-R2DBC-Multiple-Datasource project

---
+++ SpringBoot-R2DBC-Multiple-Datasource/src/main/java/com/tirmizee/SpringBootR2DbcMultipleDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootR2DbcMultipleDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootR2DbcMultipleDatasourceApplication.class, args);
+	}
+
+}
